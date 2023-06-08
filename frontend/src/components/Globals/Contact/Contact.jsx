be7dafe9e71c227bf8ed14681337767adc8ef638
KevinNizet <kevin.nizet@gmail.com>
2023-06-08 12:09:48 +0200

    :sparkles: Contact Form
    affinage des messages d'erreur en fonction des cas rencontrés lors de la saisie des données par l'utilisateur

--- frontend/src/components/Globals/Contact/Contact.jsx
+++ frontend/src/components/Globals/Contact/Contact.jsx
@@ -14,15 +14,18 @@ function Contact() {
   const { field: nomPrenomField } = useController({
     name: "NomPrenom",
     control,
-    rules: { required: true, maxLength: 20 },
+    rules: { required: true, maxLength: 20, pattern: /^[A-Za-z ]+$/i },
   });
 
   const { field: adresseMailField } = useController({
     name: "AdresseMail",
     control,
     rules: {
-      required: true,
-      pattern: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/i,
+      required: "Ce champ est requis",
+      pattern: {
+        value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/i,
+        message: "Merci de renseigner un email valide",
+      },
     },
   });
 
@@ -56,8 +59,18 @@ function Contact() {
           onBlur={nomPrenomField.onBlur}
           placeholder="Nom prénom"
         />
-        {errors.NomPrenom && (
+        {errors.NomPrenom?.type === "required" && (
           <span className="ContactForm_error">Ce champ est requis</span>
+        )}
+        {errors?.NomPrenom?.type === "maxLength" && (
+          <span className="ContactForm_error">
+            Ce champ est limité à 20 caractères
+          </span>
+        )}
+        {errors?.NomPrenom?.type === "pattern" && (
+          <span className="ContactForm_error">
+            Caractères alphabétiques uniquement
+          </span>
         )}
         <input
           className="ContactForm_input"
@@ -67,7 +80,10 @@ function Contact() {
           onBlur={adresseMailField.onBlur}
           placeholder="Adresse mail"
         />
-        {errors.AdresseMail && (
+        {errors?.AdresseMail?.type === "required" && (
+          <span className="ContactForm_error">Ce champ est requis</span>
+        )}
+        {errors?.AdresseMail?.type === "pattern" && (
           <span className="ContactForm_error">
             Merci de renseigner un email valide
           </span>
@@ -82,9 +98,14 @@ function Contact() {
           rows={4}
           cols={40}
         />
-        {errors.Message && (
+        {errors?.Message.type === "required" && (
           <span className="ContactForm_error">
             Merci d'indiquer l'objet de votre demande
+          </span>
+        )}
+        {errors?.Message?.type === "maxLength" && (
+          <span className="ContactForm_error">
+            Votre message ne peut pas exécéder 200 caractères
           </span>
         )}
         <button className="ContactForm_button" type="submit">
