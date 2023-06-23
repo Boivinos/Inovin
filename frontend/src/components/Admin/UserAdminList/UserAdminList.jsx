ae3d0cb0d1c478bcdcdfb3ce51ed75948109e2a1
KevinNizet <kevin.nizet@gmail.com>
2023-06-23 11:50:44 +0200

    ajout des boutons d'action, configuration de la navigation (à ce stade n on fonctionnelle : routes à définir. Css OK

--- frontend/src/components/Admin/UserAdminList/UserAdminList.jsx
+++ frontend/src/components/Admin/UserAdminList/UserAdminList.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import UserCard from "./UserCard";
 
 function UserAdminList() {
+  // récupréation des utilisateurs enregistrés dans la BDD
   const [userData, setUserData] = useState(undefined);
   useEffect(() => {
     axios
@@ -11,9 +13,46 @@ function UserAdminList() {
       .catch((error) => console.error(error.message));
   }, []);
 
+  // navigation vers les autres pages d'administration via les boutons
+  const navigateToAdminWinesPage = useNavigate();
+  const navigateToAdminAddUsersPage = useNavigate();
+
+  const handleUsersButtonClick = () => {
+    navigateToAdminWinesPage("/ADRESSE A DEFINIR");
+  };
+
+  const handleAddUsersButtonClick = () => {
+    navigateToAdminAddUsersPage("/ADRESSE A DEFINIR");
+  };
+
   return (
     <div className="userAdminList">
       <p id="userAdminList_title">Liste des utilisateurs </p>
+      <div className="userAdminList_buttonWrapper">
+        <button
+          id="userAdminList_usersButton"
+          type="button"
+          onClick={handleUsersButtonClick}
+        >
+          Utilisateurs
+        </button>
+        <button id="userAdminList_winesButton" type="button">
+          Vins
+        </button>
+      </div>
+      <div className="userAdminList_buttonWrapper">
+        <button className="userAdminList_button" type="button">
+          Rechercher
+        </button>
+        <button
+          className="userAdminList_button"
+          type="button"
+          onClick={handleAddUsersButtonClick}
+        >
+          Ajouter
+        </button>
+      </div>
+
       {userData &&
         userData.map((user) => {
           return (
